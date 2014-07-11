30d0d8ed4487c0cc7510d5de9b7c25b171295d47
Derek Argueta <dargueta@verisage.us>
2014-07-10 20:51:30 -0600

    Prettier and runs on Node

---
+++ public/javascripts/init.js
@@ -0,0 +1,126 @@
+// create a new player object
+var player = new Player();
+
+//this array holds the IDs of all the red blocks that kill you
+var dangerous = new Array();
+
+//this array holds the IDs of all the portals
+var portals = new Array();
+
+//turn count which can be used in the future to count how many turns the player took to complete the level
+var turnNumber = 0;
+
+//array[walls, portals]
+var levelOne = new Array("7;18;25;19;31;49;37;38;56;57;58;59;60;61;62;63;64;65;66;67;68;69;70;71;147;129;111;112;113;114;115;116;117;118;119;120;165",
+						 "51;72");
+var walls = levelOne[0].split(";");
+var levels = new Array("one", "two", "three", "four");
+var wallObjs = new Array();
+
+one();
+
+//initiate level one
+function one(){
+
+	createBack();
+	var p = levelOne[1].split(";");
+	createPortal(p[0], p[1]);
+	createWalls();
+}
+
+//this function creates the grid of grey blocks that serve as the backdrop and can be manipulated by turning them into danger blocks, portals, etc.
+function createBack(){
+
+	var t = 0;
+	var left = 0;
+
+	var numID = 0;
+	for(var x = 1; x < 181; x++){
+
+		//create the element, style it, and add it in
+		var div = document.createElement("div");
+		div.className = "empty square";
+		div.id = numID;
+		div.style.top = t + 'px';
+		div.style.left = left + 'px';
+		div.type = "Nothing";
+		document.getElementById("cage").appendChild(div);
+
+		if(x % 18 == 0){
+
+			t += 50;
+			left = 0;
+
+		} else {
+
+			left += 50;
+		}
+
+		numID++;
+	}
+}
+
+function Wall(id){
+	//creating the HTML entity
+	obj = document.getElementById(id)
+	obj.className = "wall square";
+
+	//creating the Wall object
+	this.div = document.getElementById(id);
+	this.className = "wall square";
+	this.type = "Wall";
+	this.isWall = true;
+}
+
+function createWalls(){
+	for(var x = 0; x < walls.length; x++){
+		var stuff = new Wall(walls[x]);
+		wallObjs.push(stuff);
+	}
+}
+
+//this function creates a random danger block on a valid location on the grid. Blocks can be made where the player is
+function randomDanger(){
+
+	//acquire a random number within the grid
+	var rand = Math.floor((Math.random() * 140) + 1);
+	while(rand == 0 ||
+		  inArray(rand, dangerous) ||
+		  inArray(rand, portals) ||
+		  inArray(rand, walls)) {
+
+		rand = Math.floor((Math.random() * 140 + 1));
+	}
+
+	var elem = document.getElementById(rand);
+	elem.className = "danger square";
+	elem.type = "Danger";
+	dangerous.push(rand);
+}
+
+function createPortal(portal1, portal2){
+
+	var elem1 = document.getElementById(portal1);
+	var elem2 = document.getElementById(portal2);
+	elem1.className = "portal square p1";
+	portals.push(portal1);
+	elem2.className = "portal square p2";
+	portals.push(portal2);
+
+	elem1.type = "Portal";
+	elem2.type = "Portal";
+}
+
+//function to check if a value is in an array, returns true or false
+function inArray(val, array){
+
+	for(var x = 0; x < array.length; x++){
+
+		if(array[x] == val){
+
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ public/javascripts/player.js
@@ -0,0 +1,101 @@
+/* The Player object */
+
+function Player(){
+
+    obj = $("#block");
+
+    //function to move the player's block one space to the left
+    Player.prototype.moveLeft = function(left, top){
+
+    	var wallPresent = false;
+        for(var x = 0; x < wallObjs.length; x++){
+
+        	var wallLeft = wallObjs[x].div.style.left;
+        	var wallTop = wallObjs[x].div.style.top;
+            var wallOnTop = parseInt(wallTop, 10) == parseInt(top, 10);
+            var wallOnLeft = parseInt(wallLeft, 10) + 50 == parseInt(left, 10);
+
+            if( wallOnTop && wallOnLeft){
+
+                wallPresent = true;
+            }
+        }
+        
+        if(parseInt(left, 10) > parseInt("0px", 10) && !wallPresent){
+
+            newLeftPosition = (parseInt(left,10) - parseInt("50px", 10)+"px");
+			document.getElementById("block").style.left = newLeftPosition;
+		}
+	};
+
+	// funtion to move the player's block on space to the right
+	Player.prototype.moveRight = function(left, top){
+
+        var wallPresent = false;
+        for(var x = 0; x < wallObjs.length; x++){
+
+        	var wallLeft = wallObjs[x].div.style.left;
+        	var wallTop = wallObjs[x].div.style.top;
+            var wallOnTop = parseInt(wallTop, 10) == parseInt(top, 10);
+            var wallOnLeft = parseInt(wallLeft, 10) == parseInt(left, 10) + 50;
+
+            if(wallOnTop && wallOnLeft){
+
+                wallPresent = true;
+           }
+        }
+
+        if(parseInt(left, 10) < parseInt("850px", 10) && !wallPresent){
+
+            newLeftPosition = (parseInt(left,10)+parseInt("50px", 10)+"px");
+			document.getElementById("block").style.left = newLeftPosition;
+		}
+	};
+
+	//function to move the player's block one space up
+	Player.prototype.moveUp = function(left, top){
+
+		var wallPresent = false;
+        for(var x = 0; x < wallObjs.length; x++){
+
+        	var wallLeft = wallObjs[x].div.style.left;
+        	var wallTop = wallObjs[x].div.style.top;
+            var wallOnTop = parseInt(wallTop, 10) + 50 == parseInt(top, 10);
+            var wallOnLeft = parseInt(wallLeft, 10) == parseInt(left, 10);
+
+            if(wallOnTop && wallOnLeft){
+
+                wallPresent = true;
+            }
+        }
+
+        if(parseInt(top, 10) > parseInt("0px", 10) && !wallPresent){
+
+            newTopPosition = (parseInt(top, 10) - parseInt("50px", 10)+"px");
+			document.getElementById("block").style.top = newTopPosition;
+		}
+	};
+
+	// function to move the player's block one space down
+	Player.prototype.moveDown = function(left, top){
+
+		var wallPresent = false;
+        for(var x = 0; x < wallObjs.length; x++){
+        	var wallLeft = wallObjs[x].div.style.left;
+        	var wallTop = wallObjs[x].div.style.top;
+            var wallOnTop = parseInt(wallTop, 10) == parseInt(top, 10) + 50;
+            var wallOnLeft = parseInt(wallLeft, 10) == parseInt(left, 10);
+
+            if( wallOnTop && wallOnLeft ){
+
+                wallPresent = true;
+           }
+        }
+
+        if(parseInt(top, 10) < parseInt("450px", 10) && !wallPresent){
+
+            newTopPosition = parseInt(top, 10) + parseInt("50px", 10) + "px";
+			document.getElementById("block").style.top = newTopPosition;
+		}
+	};
+}
