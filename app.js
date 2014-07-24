82cadbd490c4b9c88c4d87086c3f1d7ea72b210f
derekargueta <darguetap@gmail.com>
2014-07-23 21:29:30 -0600

    Added Bower for dependencies

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.use("/css", express.static(__dirname + '/public/stylesheets'));
 app.use("/js", express.static(__dirname + '/public/javascripts'));
+app.use('/components', express.static(__dirname + '/bower_components'));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html');
